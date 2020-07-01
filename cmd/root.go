5f774a5fb94f4f77105d7970a97258224a264914
po3rin <abctail30@gmail.com>
2020-07-01 18:52:53 +0900

    first commit

--- cmd/root.go
+++ cmd/root.go
@@ -17,14 +17,24 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
+	"time"
 
 	"github.com/spf13/cobra"
 
+	"github.com/ktr0731/go-fuzzyfinder"
 	"github.com/spf13/viper"
 )
 
 var bookmarkFile string
+
+type bookmark struct {
+	name      string
+	url       string
+	dateAdded time.Time
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -39,10 +49,41 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	b, err := ioutil.ReadFile(bookmarkFile)
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	_ = b
+
+	var tracks = []bookmark{
+		{"pon", "po3rin.com", time.Now()},
+		{"twitter", "twitter.com", time.Now()},
+		{"golang", "godoc,org", time.Now()},
+	}
+
+	id, err := fuzzyfinder.Find(
+		tracks,
+		func(i int) string {
+			return tracks[i].name
+		},
+		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			return fmt.Sprintf("%s\n\nArtist: %s\nAlbum:  %s",
+				tracks[i].name,
+				tracks[i].url,
+				tracks[i].dateAdded,
+			)
+		}),
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(tracks[id].url)
 }
 
 func init() {
@@ -52,7 +93,12 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&bookmarkFile, "file", "$HOME/Library/Application Support/Google/Chrome/Default/Bookmarks", "config file (default is $HOME/Library/Application Support/Google/Chrome/Default/Bookmarks)")
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	rootCmd.PersistentFlags().StringVar(&bookmarkFile, "file", path.Join(home, `Library/Application Support/Google/Chrome/Default/Bookmarks`), "config file (default is $HOME/Library/Application Support/Google/Chrome/Default/Bookmarks)")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/po3rin/chrbkfzf
+module github.com/po3rin/chrbmfzf
 
 go 1.14
 
 require (
+	github.com/ktr0731/go-fuzzyfinder v0.2.1
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0
 )
